7fc9bfc921080ef2a9fbe82ae992032cb45d9f54
Andrecio Costa Bezerra <andrecio@A101-36L.imd.local>
2019-11-06 20:34:34 -0300

    webpack ok

---
+++ src/book.js
@@ -0,0 +1,3 @@
+export const book = (title,author, pages,status) => {
+    return {title, author,pages,status}
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {book} from './book'
+
+
+const newBook = book('Eu','Augusto dos Anjos','666', 'Read')
+
+
